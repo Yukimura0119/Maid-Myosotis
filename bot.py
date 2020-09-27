48372f565fc26c398a188eba11896ba545a76670
Yukimura0119 <Yukimuraz0119@gmail.com>
2020-09-27 18:38:09 +0800

    new command:ping

--- bot.py
+++ bot.py
@@ -1,11 +1,31 @@
 import discord
 from discord.ext import commands
+import json
 
-bot = commands.Bot(command_prefix='[')
+with open('setting.json', 'r', encoding='utf8') as settingfile:
+    settingdata = json.load(settingfile)
+
+    
+bot = commands.Bot(command_prefix='ykt ')
 
 @bot.event
 async def on_ready():
     print(">> Bot is online <<")
 
-bot.run("NzU5NjczMDIwNjgyOTkzNjY1.X3A6og.5fOU6WTY9q0NLXo9vS_cgFLS7sU")
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(settingdata['EntranceChannelID'])
+    await channel.send(f'{member} Ya Hello!')
+
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(settingdata['EntranceChannelID'])
+    await channel.send(f'{member} Bye Bye!')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency*1000)}ms')
+
+
+bot.run(settingdata['token'])
 
