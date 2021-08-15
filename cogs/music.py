1647528650ecba09639112432eeff0f69a844ea6
YukiMingX <yukimuraz0119@gmail.com>
2021-08-16 02:18:24 +0800

    Queue system update

--- cogs/music.py
+++ cogs/music.py
@@ -1,9 +1,13 @@
-from ssl import Options
 import discord
 from discord.ext import commands
 
 import youtube_dl
 import asyncio
+import math
+import random
+import itertools
+
+from youtube_dl.utils import RegexNotFoundError
 
 from extension.cog import CogExtension
 from tools import message
@@ -24,8 +28,30 @@ ffmpeg_opts = {
 
 ydl = youtube_dl.YoutubeDL(ydl_opts)
 
-playlist = asyncio.Queue()
 next_song = asyncio.Event()
+
+
+class SongList(asyncio.Queue):
+    def __getitem__(self, item):
+        if isinstance(item, slice):
+            return list(itertools.islice(self._queue, item.start, item.stop, item.step))
+        else:
+            return self._queue[item]
+
+    def __iter__(self):
+        return self._queue.__iter__()
+
+    def __len__(self):
+        return self.qsize()
+
+    def clear(self):
+        self._queue.clear()
+
+    def shuffle(self):
+        random.shuffle(self._queue)
+
+    def remove(self, index: int):
+        del self._queue[index]
 
 
 class Music(CogExtension):
@@ -33,11 +59,12 @@ class Music(CogExtension):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.vc = None
+        self.playlist = SongList()
 
         async def audio_player_task():
             while True:
                 next_song.clear()
-                current = await playlist.get()
+                current = await self.playlist.get()
                 print(current['title'])
                 self.vc.play(
                     discord.FFmpegPCMAudio(current['url'], **ffmpeg_opts), after=lambda x: self.bot.loop.call_soon_threadsafe(next_song.set))
@@ -53,16 +80,17 @@ class Music(CogExtension):
         if ctx.voice_client is not None:
             await ctx.voice_client.disconnect()
         ch = ctx.author.voice.channel
+        await ctx.send(message.codeblock(f'connect to {ch}'))
         await ch.connect()
         self.vc = ctx.voice_client
 
     @commands.command()
     async def play(self, ctx, url: str):
-        vc = ctx.voice_client
+        if self.vc is None:
+            await self.join(ctx)
         file = ydl.extract_info(url, download=False)
         title = file['title']
-        await playlist.put(file)
-        # vc.play(discord.FFmpegPCMAudio(file['url']))
+        await self.playlist.put(file)
         await ctx.send(message.codeblock(f'ADD - {title}'))
 
     @ commands.command()
@@ -76,9 +104,33 @@ class Music(CogExtension):
         await ctx.send(message.codeblock('Song Paused.'))
 
     @ commands.command()
-    async def stop(self, ctx):
+    async def skip(self, ctx):
         ctx.voice_client.stop()
-        await ctx.send(message.codeblock('Song Stopped.'))
+        await ctx.send(message.codeblock('Song Skipped.'))
+
+    @commands.command()
+    async def clear(self, ctx):
+        self.playlist.clear()
+        await ctx.send(message.codeblock('Playlist cleared.'))
+
+    @ commands.command()
+    async def queue(self, ctx, page: int = 1):
+        if len(self.playlist) == 0:
+            return await ctx.send('Empty queue.')
+
+        items_per_page = 10
+        pages = math.ceil(len(self.playlist) / items_per_page)
+
+        start = (page - 1) * items_per_page
+        end = start + items_per_page
+
+        queue = ''
+        for i, song in enumerate(self.playlist[start:end], start=start):
+            queue += '`{}.` {}\n'.format(i + 1, song['title'])
+
+        embed = (discord.Embed(description='**{} tracks:**\n\n{}'.format(len(self.playlist), queue))
+                 .set_footer(text='Viewing page {}/{}'.format(page, pages)))
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
